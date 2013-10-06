73d666e849e42aaa2e578c8d35d3eca2bd7e36d2
Archon- <archon@archons.info>
2013-10-06 16:49:16 +0200

    Time elapse
    Measuring time between two points in program.

---
+++ Time-elapse/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    long int before = GetTickCount();
+
+    // Perform time-consuming operation
+    Sleep(3000);
+    cout<<"blablabla"<<endl;
+    for(unsigned int i = 0; i < 10000000; i++){                     
+    }
+    
+    long int after = GetTickCount();
+    cout<<"before: "<<before<<", after: "<<after<<endl;
+    cout<<"diff time: "<<(after-before)<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
