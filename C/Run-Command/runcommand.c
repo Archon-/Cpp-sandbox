08fbc5cc245e43cf0fa22cb5718a2ba9970c93ca
Archon- <archon@archons.info>
2013-10-22 00:49:10 +0200

    Run Command
    Run command line from program and get output.

---
+++ C/Run-Command/runcommand.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    FILE * fptr;                    // file holder
+    char c;                         // char buffer
+
+
+    //system("dir >> temp.txt");      // call dir and put it's contents in a temp using redirects.
+    system("dig google.pl");
+    fptr = fopen("temp.txt", "r");  // open said file for reading.
+                                    // oh, and check for fptr being NULL.
+    while(1){
+        c = fgetc(fptr);
+        if(c!= EOF)
+            printf("%c", c);        // do what you need to.
+        else
+            break;                  // exit when you hit the end of the file.
+    }
+    fclose(fptr);                   // don't call this is fptr is NULL.  
+    remove("temp.txt");             // clean up
+
+    getchar();                      // stop so I can see if it worked.
+}
