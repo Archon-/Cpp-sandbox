8e160b70f9c297835b96563d13ec825891ab08ad
Archon- <archon@archons.info>
2013-11-27 01:29:08 +0100

    Min value in array

---
+++ min_val/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int array[100], minimum, size, c, location = 1;
+ 
+    printf("Enter the number of elements in array\n");
+    scanf("%d",&size);
+ 
+    printf("Enter %d integers\n", size);
+ 
+    for ( c = 0 ; c < size ; c++ )
+        scanf("%d", &array[c]);
+ 
+    minimum = array[0];
+ 
+    for ( c = 1 ; c < size ; c++ ) 
+    {
+        if ( array[c] < minimum ) 
+        {
+           minimum = array[c];
+           location = c+1;
+        }
+    }
+    
+    printf("Smallest numer val: %d and position: %d\n", minimum, location);
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
